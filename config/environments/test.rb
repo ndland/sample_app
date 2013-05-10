2a34f6b5f898057ee06d02a50940bb619bca5b51
Nicholas Land <nland22@gmail.com>
2013-05-10 17:52:17 -0400

    WIP: 8.3.1 Installation and Setup of Cucumber

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,7 +36,7 @@ SampleApp::Application.configure do
   config.active_support.deprecation = :stderr
 
   # speed up tests by lower BCrypt's cost function.
-  require 'BCrypt'
+require 'bcrypt'
   silence_warnings do
     BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
   end
