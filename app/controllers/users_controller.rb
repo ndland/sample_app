b49fd20f2bbc189422438e04391820657cec14d8
Nicholas Land <nland22@gmail.com>
2013-05-13 11:16:01 -0400

    Finished up Authorization for updating profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+    before_filter :signed_in_user, only: [:edit, :update]
+
 	def show
 
 		@user = User.find(params[:id])
@@ -37,4 +39,11 @@ class UsersController < ApplicationController
         render 'edit'
     end      
   end
+
+  private
+
+    def signed_in_user
+        
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
