f408c5c3e5cb5b8c8ce80899047b368de9843d5f
Nicholas Land <nland22@gmail.com>
2013-05-13 13:43:26 -0400

    Added an index for all users at '/users', only viewable if you're logged into the site. If you're not logged into the site, will redirect to the sign in page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-    before_filter :signed_in_user,  only: [:edit, :update]
+    before_filter :signed_in_user,  only: [:index, :edit, :update]
     before_filter :correct_user,    only: [:edit, :update]
 
 	def show
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
         render 'edit'
     end      
   end
+
+    def index
+       @users = User.all
+    end
 
   private
 
